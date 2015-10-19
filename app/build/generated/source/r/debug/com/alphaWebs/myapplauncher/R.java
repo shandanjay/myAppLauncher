961750a470c23d0d1adadad4dc7322946bd7a110
shandanjay <shandanjay@gmail.com>
2015-10-19 15:51:15 +0530

    2nd push

--- app/build/generated/source/r/debug/com/alphaWebs/myapplauncher/R.java
+++ app/build/generated/source/r/debug/com/alphaWebs/myapplauncher/R.java
@@ -18,6 +18,7 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int launcher_button=0x7f020001;
+        public static final int launcher_buttons=0x7f020002;
     }
     public static final class id {
         public static final int app1=0x7f070001;
--- app/src/main/java/com/alphaWebs/myapplauncher/MainActivity.java
+++ app/src/main/java/com/alphaWebs/myapplauncher/MainActivity.java
@@ -25,8 +25,11 @@ public class MainActivity extends Activity
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        Button b = (Button)view;
+                        String btnText =  b.getText().toString();
                         Context context = getApplicationContext();
-                        CharSequence text = "Hello toast!";
+
+                        CharSequence text = (CharSequence) "This button will launch my app called "+btnText;
                         int duration = Toast.LENGTH_SHORT;
 
                         Toast toast = Toast.makeText(context, text, duration);
@@ -35,6 +38,108 @@ public class MainActivity extends Activity
                 }
 
         );
+
+        Button app2i = (Button)findViewById(R.id.app2i);
+        app2i.setOnClickListener(
+
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Button b = (Button)view;
+                        String btnText =  b.getText().toString();
+                        Context context = getApplicationContext();
+
+                        CharSequence text = (CharSequence) "This button will launch my app called "+btnText;
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                }
+
+        );
+
+        Button app2ii = (Button)findViewById(R.id.app2ii);
+        app2ii.setOnClickListener(
+
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Button b = (Button)view;
+                        String btnText =  b.getText().toString();
+                        Context context = getApplicationContext();
+
+                        CharSequence text = (CharSequence) "This button will launch my app called "+btnText;
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                }
+
+        );
+
+        Button app3 = (Button)findViewById(R.id.app3);
+        app3.setOnClickListener(
+
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Button b = (Button)view;
+                        String btnText =  b.getText().toString();
+                        Context context = getApplicationContext();
+
+                        CharSequence text = (CharSequence) "This button will launch my app called "+btnText;
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                }
+
+        );
+
+        Button app4 = (Button)findViewById(R.id.app4);
+        app4.setOnClickListener(
+
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Button b = (Button)view;
+                        String btnText =  b.getText().toString();
+                        Context context = getApplicationContext();
+
+                        CharSequence text = (CharSequence) "This button will launch my app called "+btnText;
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                }
+
+        );
+
+        Button app5 = (Button)findViewById(R.id.app5);
+        app5.setOnClickListener(
+
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Button b = (Button)view;
+                        String btnText =  b.getText().toString();
+                        Context context = getApplicationContext();
+
+                        CharSequence text = (CharSequence) "This button will launch my app called "+btnText;
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                }
+
+        );
+
     }
+
 
 }
